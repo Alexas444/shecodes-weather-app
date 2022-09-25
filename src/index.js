234d98f93e1c987fdc63ac9b99f7d0a8d9c04d53
Alexas444 <113315714+Alexas444@users.noreply.github.com>
2022-09-24 21:32:28 -0400

    added "feels like" property

--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@ function showWeatherToday(response) {
   document.querySelector("#displayed-city").innerHTML = response.data.name;
   document.querySelector("#temp-number").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#weather-info-adj-today").innerHTML = response.data.weather[0].main;
+  document.querySelector("#feels-like-today").innerHTML = Math.round(response.data.main.feels_like) + `℃`;
   document.querySelector("#humidity-today").innerHTML = response.data.main.humidity;
   document.querySelector("#wind-today-km").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#displayed-country").innerHTML = regionNames.of(response.data.sys.country);
